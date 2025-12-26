47c4fdf3f3774bf1686fa6dc3ff74f0056edd83a
Just-NoWorking322 <alisherbolotbekov03@gmail.com>
2025-12-26 23:19:54 +0600

    views --> class, management

---
+++ README.md
@@ -0,0 +1,14 @@
+"""
+Проект: Беш-Ташта
+
+>----- Ориентир -----<
+
+Management - приложение для контроля средствами Расходы Доходы Отчеты и статистика
+Users - Пользователи и Профиль
+
+>-- - -- -- Figma -- - -- -- <
+https://www.figma.com/design/HodhFz2wvDMKnWUpoTElmY/Besh-Tashta?node-id=0-1&p=f&t=MUyufL45D6jUE3sG-0
+
+>-- - -- -- Git Hub -- -- - --<
+https://github.com/Just-NoWorking322/Android
+"""
--- apps/users/views.py
+++ apps/users/views.py
@@ -3,58 +3,61 @@ from django.contrib.auth import login
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import messages
 from .forms import UserRegistrationForm, UserProfileForm
-from .models import User, UserProfile, Privilege, UserPrivilege
 from django.contrib.auth.decorators import login_required
+from django.views.generic import CreateView, UpdateView, ListView, View, TemplateView
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Privilege, UserPrivilege
 
+class UserRegisterView(CreateView):
+    form_class = UserRegistrationForm
+    template_name = 'users/register.html'
+    success_url = reverse_lazy('profile')
 
-def register(request):
-    """Регистрация пользователя"""
-    if request.method == 'POST':
-        form = UserRegistrationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            messages.success(request, 'Вы успешно зарегистрированы!')
-            return redirect('profile')
-    else:
-        form = UserRegistrationForm()
-    return render(request, 'users/register.html', {'form': form})
+    def form_valid(self, form):
+        """Если форма валидна, сохраняем пользователя и логиним"""
+        user = form.save()
+        login(self.request, user)
+        messages.success(self.request, 'Вы успешно зарегистрированы!')
+        return redirect(self.success_url)
 
+class UserProfileView(LoginRequiredMixin, UpdateView):
+    form_class = UserProfileForm
+    template_name = 'users/profile.html'
+    success_url = reverse_lazy('profile')
 
-def profile(request):
-    """Профиль пользователя"""
-    if request.method == 'POST':
-        profile_form = UserProfileForm(request.POST, request.FILES, instance=request.user.userprofile)
-        if profile_form.is_valid():
-            profile_form.save()
-            messages.success(request, 'Профиль успешно обновлен!')
-            return redirect('profile')
-    else:
-        profile_form = UserProfileForm(instance=request.user.userprofile)
+    def get_object(self, queryset=None):
+        """Получаем профиль текущего пользователя"""
+        return self.request.user.userprofile
 
-    return render(request, 'users/profile.html', {'profile_form': profile_form})
+    def form_valid(self, form):
+        """Если форма валидна, сохраняем изменения и показываем сообщение"""
+        messages.success(self.request, 'Профиль успешно обновлен!')
+        return super().form_valid(form)
 
-@login_required
-def privilege_list(request):
-    """Страница с доступными привилегиями для покупки"""
-    privileges = Privilege.objects.all()  # Получаем все привилегии
-    return render(request, 'users/privilege_list.html', {'privileges': privileges})
+class PrivilegeListView(LoginRequiredMixin, ListView):
+    model = Privilege
+    template_name = 'users/privilege_list.html'
+    context_object_name = 'privileges'
 
-@login_required
-def buy_privilege(request, privilege_id):
-    """Покупка привилегии пользователем"""
-    privilege = Privilege.objects.get(id=privilege_id)
-    
-    if UserPrivilege.objects.filter(user=request.user, privilege=privilege).exists():
-        messages.warning(request, 'Вы уже купили эту привилегию.')
+class BuyPrivilegeView(LoginRequiredMixin, View):
+    def post(self, request, privilege_id):
+        privilege = Privilege.objects.get(id=privilege_id)
+
+        if UserPrivilege.objects.filter(user=request.user, privilege=privilege).exists():
+            messages.warning(request, 'Вы уже купили эту привилегию.')
+            return redirect('privilege_list')
+
+        UserPrivilege.objects.create(user=request.user, privilege=privilege)
+        messages.success(request, f'Вы успешно купили привилегию "{privilege.name}".')
+
         return redirect('privilege_list')
+
+class UserProfileWithPrivilegesView(LoginRequiredMixin, TemplateView):
+    template_name = 'users/profile.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['user_privileges'] = UserPrivilege.objects.filter(user=self.request.user)
+        return context
     
-    UserPrivilege.objects.create(user=request.user, privilege=privilege)
-    messages.success(request, f'Вы успешно купили привилегию "{privilege.name}".')
-    
-    return redirect('privilege_list')
-@login_required
-def profile(request):
-    """Профиль пользователя"""
-    user_privileges = UserPrivilege.objects.filter(user=request.user)  
-    return render(request, 'users/profile.html', {'user_privileges': user_privileges})
